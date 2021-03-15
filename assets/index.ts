eda2f763f1660458adf467bca4f9bce24c4f1b88
Javier Manobanda <51manobandajavier@gmail.com>
2021-03-15 09:39:16 -0500

    onvert project to typescript

--- assets/index.ts
+++ assets/index.ts
@@ -1,10 +1,10 @@
-import MediaPlayer from "./mediaPlayer.js";
-import AutoPlay from "../plugins/AutoPlay.js";
-import AutoPause from "../plugins/AutoPause.ts";
+import MediaPlayer from "./mediaPlayer";
+import AutoPlay from "../plugins/AutoPlay";
+import AutoPause from "../plugins/AutoPause";
 
 const video = document.querySelector("video");
-const button = document.querySelector("#btnPlay");
-const buttonMute = document.querySelector("#btnMute");
+const button: HTMLElement = document.querySelector("#btnPlay");
+const buttonMute: HTMLElement = document.querySelector("#btnMute");
 
 const player = new MediaPlayer({
   el: video,
--- assets/mediaPlayer.ts
+++ assets/mediaPlayer.ts
@@ -1,28 +1,17 @@
 class MediaPlayer {
+  media:HTMLMediaElement
+  plugins: Array<any>
+
   constructor(config) {
     this.media = config.el;
     this.plugins = config.plugins || [];
 
-    this._initPlugins();
+    this.initPlugins();
   }
 
-  _initPlugins() {
-    /* solo le das acceso aciertas cosas del objeto player */
-    const player = {
-      play: () => this.play(),
-      pause: () => this.pause(),
-      media: this.media,
-      get muted() {
-        return this.media.muted;
-      },
-
-      set muted(value) {
-        this.media.muted = value;
-      },
-    };
-
+  private initPlugins() {
     this.plugins.forEach((plugin) => {
-      plugin.run(player);
+      plugin.run(this);
     });
   }
 
